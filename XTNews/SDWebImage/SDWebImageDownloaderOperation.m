51a691df58586dee27f6718b4de99a447d6c26bd
Tongtong Xu <xutongtong@breadtrip.com>
2015-01-09 11:16:21 +0800

    修改SDWebImage

--- XTNews/SDWebImage/SDWebImageDownloaderOperation.m
+++ XTNews/SDWebImage/SDWebImageDownloaderOperation.m
@@ -36,6 +36,8 @@
     BOOL responseFromCached;
 }
 
+@synthesize executing = _executing,finished = _finished;
+
 - (id)initWithRequest:(NSURLRequest *)request options:(SDWebImageDownloaderOptions)options progress:(void (^)(NSInteger, NSInteger))progressBlock completed:(void (^)(UIImage *, NSData *, NSError *, BOOL))completedBlock cancelled:(void (^)())cancelBlock {
     if ((self = [super init])) {
         _request = request;
